4268bd354a4cdfe9fd86c6c9598b18cf1ab49708
creanf <creanf@rpi.edu>
2025-01-18 16:31:00 -0500

    extra

--- Source/FightingGame/GameMode_Default.cpp
+++ Source/FightingGame/GameMode_Default.cpp
@@ -3,3 +3,55 @@
 
 #include "GameMode_Default.h"
 
+#include "HUD_Crosshair.h"
+#include "PlayerPawn.h" //custom classes
+#include "GameFramework/PlayerStart.h"
+
+#include "Engine/World.h"
+#include "Kismet/GameplayStatics.h"
+
+AGameMode_Default::AGameMode_Default()
+{
+    // Set default classes (assign your custom ones here if needed)
+    DefaultPawnClass = APlayerPawn::StaticClass();  
+    HUDClass = AHUD_Crosshair::StaticClass();         
+}
+
+void AGameMode_Default::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void AGameMode_Default::HandleStartingNewPlayer_Implementation(APlayerController* NewPlayer)
+{
+    Super::HandleStartingNewPlayer_Implementation(NewPlayer);
+
+    // Example: Custom logic for initializing a new player
+    if (NewPlayer && NewPlayer->GetPawn() == nullptr)
+    {
+        AActor* SpawnLocation = ChoosePlayerStart(NewPlayer);
+        if (SpawnLocation)
+        {
+            APawn* NewPawn = GetWorld()->SpawnActor<APawn>(DefaultPawnClass, SpawnLocation->GetActorLocation(), SpawnLocation->GetActorRotation());
+            if (NewPawn)
+            {
+                NewPlayer->Possess(NewPawn);
+                UE_LOG(LogTemp, Log, TEXT("New player spawned and possessed a pawn."));
+            }
+        }
+    }
+}
+
+AActor* AGameMode_Default::ChoosePlayerStart_Implementation(AController* PlayerController)
+{
+    // Example: Find a random PlayerStart actor
+    TArray<AActor*> PlayerStarts;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerStart::StaticClass(), PlayerStarts);
+
+    if (PlayerStarts.Num() > 0)
+    {
+        return PlayerStarts[FMath::RandRange(0, PlayerStarts.Num() - 1)];
+    }
+
+    return Super::ChoosePlayerStart_Implementation(PlayerController);
+}
--- Source/FightingGame/GameMode_Default.h
+++ Source/FightingGame/GameMode_Default.h
@@ -7,11 +7,27 @@
 #include "GameMode_Default.generated.h"
 
 /**
+ * Multiplayer GameMode with custom functionality for player spawning and session management.'
  * 
+ * This is a GameMode. GameModes decide which playerPawn class, HUD class, replay class etc. that the engine uses when you start the game.
+ * 
+ * This class also allows new players to spawn at the location of an existing player
  */
 UCLASS()
 class FIGHTINGGAME_API AGameMode_Default : public AGameMode
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    AGameMode_Default();
+
+protected:
+    // Override to handle player initialization
+    virtual void HandleStartingNewPlayer_Implementation(APlayerController* NewPlayer) override;
+
+    // Override to control where players spawn
+    virtual AActor* ChoosePlayerStart_Implementation(AController* PlayerController) override;
+
+    // Function to initialize custom game state
+    virtual void BeginPlay() override;
 };
